fe5f991b5159217b86b121a82b5b6455d23817ca
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2019-01-09 00:22:03 +0800

    remove coding repos

--- synctocoding.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone -b master "https://kmahyyg:${GITHUB_PERTKN}@github.com/kmahyyg/kmahyyg.github.io.git" /tmp/buld_y && cd /tmp/buld_y && git push --force "https://kmahyyg:${CODING_PERTKN}@${CO_REF}" master
-echo "Return status code $?"
-exit 0
