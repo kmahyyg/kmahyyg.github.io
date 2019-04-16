ccbd71e41e14d188fcbc6d30b8524087ab3c7af3
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2019-04-16 10:42:50 +0800

    correct thinking error

--- .download-deps.sh
+++ .download-deps.sh
@@ -10,9 +10,9 @@ if [ -f "$HUGODEB" ]; then
     CURSHA2DEB=`sha256sum ${HUGODEB} | awk '{print $1}'`
     if [ ${CURSHA2DEB} == ${STASHA2DEB} ]; then
         echo "Hugo 0.54 Debian Package Checksum correct and cache worked."
-        exit 0
     fi
 else 
     wget --no-check-certificate https://github.com/gohugoio/hugo/releases/download/v0.54.0/hugo_0.54.0_Linux-64bit.deb -O ${HUGODEB}
-    sudo dpkg -i ${HUGODEB}
 fi
+
+sudo dpkg -i ${HUGODEB}
